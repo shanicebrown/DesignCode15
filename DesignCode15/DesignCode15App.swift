a96c48aa99bce24bf558d7b8d2f52e266eaf8b53
Shanice Brown <s.mariebrown10@gmail.com>
2021-12-11 09:57:07 -0600

    Initial Commit

---
+++ DesignCode15/DesignCode15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode15App.swift
+//  DesignCode15
+//
+//  Created by Shanice Brown on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
